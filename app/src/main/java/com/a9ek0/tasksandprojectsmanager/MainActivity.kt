699eddc38c25a455c4658631aa17c349bbd1181e
a9ek0 <109074282+a9ek0@users.noreply.github.com>
2024-09-26 05:01:03 +0300

    Сalendar page designed

--- app/src/main/java/com/a9ek0/tasksandprojectsmanager/MainActivity.kt
+++ app/src/main/java/com/a9ek0/tasksandprojectsmanager/MainActivity.kt
@@ -1,47 +1,31 @@
 package com.a9ek0.tasksandprojectsmanager
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.a9ek0.tasksandprojectsmanager.ui.theme.TasksAndProjectsManagerTheme
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            TasksAndProjectsManagerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+
+        val hoursContainer = findViewById<LinearLayout>(R.id.hours_container)
+
+        for (hour in 0..23) {
+            val hourView = layoutInflater.inflate(R.layout.hour_block, hoursContainer, false)
+            val hourText = hourView.findViewById<TextView>(R.id.hour_text)
+
+            val timeString = when {
+                hour == 0 -> "12 AM"
+                hour < 12 -> "$hour AM"
+                hour == 12 -> "12 PM"
+                else -> "${hour - 12} PM"
             }
+
+            hourText.text = timeString
+            hoursContainer.addView(hourView)
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TasksAndProjectsManagerTheme {
-        Greeting("Android")
     }
 }
